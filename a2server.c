14b4b630dd452ee69a7fc124a2dfafde079c5648
Lawrence Wong <lawrencecywong@gmail.com>
2015-02-21 20:01:31 -0500

    Correcting null

--- a2server.c
+++ a2server.c
@@ -37,7 +37,7 @@ int main(int argc, char**argv)
    {
       clientQueues[i] = malloc(sizeof(ticketNode));
       clientQueues[i]->pid = 0;
-      clientQueues[i]->requestType = NULL;
+      clientQueues[i]->requestType = '';
       clientQueues[i]->length = 0;
       clientQueues[i]->head = NULL;
       clientQueues[i]->next = NULL;
